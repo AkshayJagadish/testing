61625fd49a238006ee3362ec4e0f61b9300e9b0c
Akshay Jagadish <akshaysajnani1996@gmail.com>
2020-09-30 01:58:51 -0400

    Update genpy.py

--- .github/workflows/genpy.py
+++ .github/workflows/genpy.py
@@ -1,4 +1,6 @@
-filepath = './.github/workflows/myfile2.txt'
-with open(filepath, 'a+') as fp:
-    line = fp.readline()
-    print(line)
+filepath = './.github/workflows/myfile2.txt'    
+with open(filepath, "a+") as f:
+    data = f.read()
+    f.seek(0)
+    f.write(output)
+    f.truncate()    
